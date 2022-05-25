2ef49db1d258c274d96703f8c880a4452dcbac7d
Revive-dontwanttocode <82950452+Revive-dontwanttocode@users.noreply.github.com>
2022-05-25 19:58:04 +0800

    Update git.java

--- src/main/java/com/czx/git.java
+++ src/main/java/com/czx/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("Hello master!");
         System.out.println("hot-fix test!");
         System.out.println("Push test!");
+        System.out.println("Pull test!");
     }
 }
