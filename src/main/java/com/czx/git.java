dc7ebf33c7bcaf9ad21f7c6f0c727bb6378e96ae
revive <chenyu102533@gmail.com>
2022-05-25 17:26:56 +0800

    Second commit...

--- src/main/java/com/czx/git.java
+++ src/main/java/com/czx/git.java
@@ -3,5 +3,6 @@ package com.czx;
 public class git {
     public static void main(String[] args) {
         System.out.println("Bonjour!");
+        System.out.println("Bonjour!Second...");
     }
 }
